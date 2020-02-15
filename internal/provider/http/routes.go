b8ec65f4ca61037dc41c1b03f1a6aaab47b2d771
luannevesb <luan29.barroso@gmail.com>
2020-02-15 00:25:44 -0300

    UPDATE - Setando rotas de account

--- internal/provider/http/routes.go
+++ internal/provider/http/routes.go
@@ -1,9 +1,20 @@
 package http
 
 import (
+	"github.com/gorilla/mux"
 	"github.com/luannevesb/challenge-stone-accounts/internal/service"
+	"log"
+	"net/http"
 )
 
 func InitRouter (service *service.Service) {
-	//TODO DECLARE ROUTES
+	//Inicia o a dependência MUX
+	router := mux.NewRouter()
+
+	//Inicia as rotas de accounts e informa qual método vai receber qual chamada
+	router.HandleFunc("/accounts", service.GetAllAccounts).Methods("GET")
+	router.HandleFunc("/accounts/{id}", service.GetAccount).Methods("GET")
+	router.HandleFunc("/accounts", service.CreateAccounts).Methods("POST")
+
+	log.Fatal(http.ListenAndServe(":8000", router))
 }
